22275579bf6b12e252cc15fac210dff4be569011
corey255a1 <wundervision2013@gmail.com>
2024-04-14 06:48:52 -0400

    Clean up RaspiCam

--- camera/raspilapse_cam.py
+++ camera/raspilapse_cam.py
@@ -9,30 +9,21 @@ from picamera import PiCamera
 from picamera.array import PiRGBArray
 from datetime import datetime
 import numpy as np
-RASPI_SVR = "http://192.168.1.125:8989"
 
-
-def register():
-   url = RASPI_SVR + "/register"
-   print(url)
-   try:
-     response = urllib.request.urlopen(url)
-     print(response)
-     if response.getcode() == 200:
-       return 0
-     else:
-       return -2
-   except urllib.error.URLError as e:
-     return -1
-
+## Camera Control
+background_subtractor = None
+detection_region_tl = (400, 200)
+detection_region_br = (1200, 1000)
 def time_stamp(image):
    text = str(datetime.now())
    font = cv2.FONT_HERSHEY_SIMPLEX
    font_scale = 1
    font_color = (125, 125, 10)  # bluegreen color
    thickness = 4
-   cv2.putText(image, text, (2000, 1800), font, font_scale, font_color, thickness)
+   cv2.putText(image, text, (1300, 1100), font, font_scale, font_color, thickness)
 
+def apply_region(frame):
+    return cv2.rectangle(frame, detection_region_tl, detection_region_br, (127,127,127), 10)
 
 def jpeg_encode(frame):
     return cv2.imencode(".jpg", frame)[1]
@@ -63,15 +54,24 @@ def grab_photo():
     time_stamp(frame)
     return jpeg_encode(frame)
 
-app = Flask(__name__)
-background_subtractor = cv2.createBackgroundSubtractorMOG2()
-detection_region_tl = (400, 200)
-detection_region_br = (1200, 1000)
+def update_background_image(count):
+    global background_subtractor
+    for frame in get_raw_image_sequence(count):
+       background_subtractor.apply(frame)
+    return background_subtractor
 
+def set_background_image(initial_count):
+    # Create a new background subtractor
+    global background_subtractor
+    background_subtractor = cv2.createBackgroundSubtractorMOG2()
+    return update_background_image(initial_count)
 
-@app.route("/get_photo")
-def get_photo():
-    return Response(grab_photo().tostring(), content_type='image/jpeg')
+def set_detection_region(x1, y1, x2, y2):
+    global detection_region_tl
+    global detection_region_br
+    detection_region_tl = (x1, y1)
+    detection_region_br = (x2, y2)
+    return {"x1":x1, "y1":y1, "x2":x2, "y2":y2}
 
 def get_diff_image():
    global background_subtractor
@@ -82,44 +82,35 @@ def get_diff_image():
    foreground = cv2.erode(foreground, kernel)
    return foreground
 
-def apply_region(frame):
-    return cv2.rectangle(frame, detection_region_tl, detection_region_br, (127,127,127), 10)
+def get_contour_count():
+   foreground = get_diff_image()
+   region_of_interest = foreground[detection_region_tl[1]:detection_region_br[1], detection_region_tl[0]:detection_region_br[0]]
+   _ , threshold = cv2.threshold(region_of_interest, 30, 255, cv2.THRESH_BINARY)
+   contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+   return len(contours)
+
+
+## Routes
+app = Flask(__name__)
+
+@app.route("/get_photo")
+def get_photo():
+    return Response(grab_photo().tostring(), content_type='image/jpeg')
 
 @app.route("/get_diff_image")
 def handle_get_diff_image():
    return Response(jpeg_encode(apply_region(get_diff_image())).tostring(), content_type='image/jpeg')
-
-def set_background_image(initial_count):
-    # Create a new background subtractor
-    global background_subtractor
-    background_subtractor = cv2.createBackgroundSubtractorMOG2()
-    for frame in get_raw_image_sequence(initial_count):
-       background_subtractor.apply(frame)
-    return background_subtractor
 
 @app.route("/set_background_image")
 def handle_set_background_image():
     subtractor = set_background_image(4)
     return Response(jpeg_encode(subtractor.getBackgroundImage()).tostring(), content_type='image/jpeg')
 
-def update_background_image(count):
-    global background_subtractor
-    for frame in get_raw_image_sequence(count):
-       background_subtractor.apply(frame)
-    return background_subtractor
-
 @app.route("/update_background_image")
 def handle_update_background_image():
     print("updating background")
     subtractor = update_background_image(4)
     return Response(jpeg_encode(subtractor.getBackgroundImage()).tostring(), content_type='image/jpeg')
-
-def set_detection_region(x1, y1, x2, y2):
-    global detection_region_tl
-    global detection_region_br
-    detection_region_tl = (x1, y1)
-    detection_region_br = (x2, y2)
-    return {"x1":x1, "y1":y1, "x2":x2, "y2":y2}
 
 @app.route("/set_detection_region")
 def handle_set_detection_region():
@@ -129,29 +120,34 @@ def handle_set_detection_region():
                           int(request.args.get('y2')))
     return jsonify(region);
 
-def get_contour_count():
-   foreground = get_diff_image()
-   region_of_interest = foreground[detection_region_tl[1]:detection_region_br[1], detection_region_tl[0]:detection_region_br[0]]
-   _ , threshold = cv2.threshold(region_of_interest, 30, 255, cv2.THRESH_BINARY)
-   contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-   return len(contours)
-
 @app.route("/is_object_detected")
 def handle_is_object_detected():
     print("handling object detection")
     return jsonify({"detected":get_contour_count()})
-
 
 @app.route("/status")
 def get_status():
    return jsonify({"status":"good"});
 
 
-
 if __name__ == "__main__":
     print("Initializing Camera")
-    for frame in get_raw_image_sequence(4):
-       background_subtractor.apply(frame)
+    set_background_image(4)
     print("Starting Server")
     app.run(host='0.0.0.0', port=7070, debug=False)
 
+
+# For auto registering potentially in the future...
+# RASPI_SVR = "http://192.168.1.125:8989"
+# def register():
+#    url = RASPI_SVR + "/register"
+#    print(url)
+#    try:
+#      response = urllib.request.urlopen(url)
+#      print(response)
+#      if response.getcode() == 200:
+#        return 0
+#      else:
+#        return -2
+#    except urllib.error.URLError as e:
+#      return -1
